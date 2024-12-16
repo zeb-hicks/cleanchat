d4cd3ed1c101137af3b4b9a95a0b51fa97c52e6c
zeb-hicks <Nimphious@gmail.com>
2024-12-16 22:09:51 +1100

    Support both manifest V2 and V3

--- package.sh
+++ package.sh
@@ -13,9 +13,13 @@ if [ -z "$VERSION" ]; then
 fi
 
 # Create a zip file with the version number
-ZIP_FILE="cleanchat_$VERSION.zip"
+ZIP_FILE="cleanchat_$VERSION"
 
 # Add icons and scripts to the zip file
-zip -r "$ZIP_FILE" icon*.png script.js style.css manifest.json
+cp -y manifestv3.json manifest.json
+zip -r "$ZIP_FILE.v3.zip" icon*.png script.js style.css manifest.json
+cp -y manifestv2.json manifest.json
+zip -r "$ZIP_FILE.v2.zip" icon*.png script.js style.css manifest.json
+rm manifest.json
 
 echo "Created $ZIP_FILE with icons and scripts"
